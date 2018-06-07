e769c2af22da241a3d12e6dc34b6f7cf81f11e4e
Travis <travisjw@gmail.com>
2018-06-06 23:49:27 -0700

    challenge 4 done

---
+++ codewars4.js
@@ -0,0 +1,11 @@
+function insertDash(num) {
+  num = num.toString();
+  var newArray = [];
+  for (var i=0; i<num.length; i++){
+    newArray.push(num[i]);
+    if (num[i] % 2 && num[i+1] %2){
+      newArray.push('-');
+     }
+   }
+  return newArray.join('');
+}
